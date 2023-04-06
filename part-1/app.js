d931cd713508faaac2c24ebf303826e01974f8a0
Jingbo Yang <58735928+KimperYang@users.noreply.github.com>
2023-04-05 23:29:02 -0500

    Update app.js

--- part-1/app.js
+++ part-1/app.js
@@ -1,11 +1,10 @@
 // Set constraints for the video stream
-var constraints = {
-  audio: true,
-  video: {
-    facingMode: { exact: "environment" },
-  },
-};
-// var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: { exact: "environment" },
+width: { ideal: 4096 },
+height: { ideal: 2160 } }, audio: false };
+// var constraints = { video: { facingMode: "user" ,
+// width: { ideal: 4096 },
+// height: { ideal: 2160 } }, audio: false };
 var track = null;
 
 // Define constants
@@ -14,6 +13,8 @@ const cameraView = document.querySelector("#camera--view"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger");
 
+var radio=document.getElementsByName("quality");
+var selectvalue=null;
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -29,11 +30,21 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+      for(var i=0;i<radio.length;i++){
+        if(radio[i].checked==true) {
+                 selectvalue=radio[i].value;
+                 break;
+        }
+      }
+    console.log(selectvalue) // selectValue 0 represents normal while 1 represents abnormal
+    if(selectvalue == null){alert("Select the type of dashboard first!")}
+    else{
+      cameraSensor.width = cameraView.videoWidth;
+      cameraSensor.height = cameraView.videoHeight;
+      cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+      cameraOutput.src = cameraSensor.toDataURL("image/webp");
+      cameraOutput.classList.add("taken");
+    }
     // track.stop();
 };
 
